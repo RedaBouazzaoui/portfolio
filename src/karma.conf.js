5b015864e13a9cc5bc4c91ad26470312a191415d
RdaBouazzaoui <reda.bouazaoui10@gmail.com>
2020-07-28 20:38:44 -0400

    fixed karma file

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,11 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/string';
-import 'core-js/es6/array';
 
-import 'intl';
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -17,20 +12,21 @@ module.exports = function (config) {
     //   require('karma-coverage-istanbul-reporter'),
     //   require('@angular-devkit/build-angular/plugins/karma')
     // ],
-    // plugins: [
-    //   require('karma-jasmine'),
-    //   require('karma-firefox-launcher'),
-    //   require('karma-jasmine-html-reporter'),
-    //   require('karma-coverage-istanbul-reporter'),
-    //   require('@angular-devkit/build-angular/plugins/karma')
-    // ],
     plugins: [
-      require( 'karma-jasmine' ),
-      require( 'karma-chrome-launcher' ),
-      require( 'karma-phantomjs-launcher' ),
-      require( 'karma-remap-istanbul' ),
-      require( 'angular-cli/plugins/karma' )
-  ],
+      require('karma-jasmine'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require( 'karma-phantomjs-launcher' )
+    ],
+  //   plugins: [
+  //     require( 'karma-jasmine' ),
+  //     require( 'karma-chrome-launcher' ),
+  //     require( 'karma-phantomjs-launcher' ),
+  //     require( 'karma-remap-istanbul' ),
+  //     require( 'angular-cli/plugins/karma' )
+  // ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
